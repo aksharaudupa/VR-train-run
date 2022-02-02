fa8fc9abf2211670b8fe96d1253fe8c6d81bd207
aksharaudupa <aksharaudupa@gmail.com>
2022-02-02 23:52:08 +0530

    Player changes

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -33,15 +33,48 @@ public class Player : MonoBehaviour {
 		} else if (transform.position.x > 1.75f) {
 			transform.position = new Vector3 (1.75f, transform.position.y, transform.position.z);
 		}
-        if(!isDead)
-		    scoreText.text = "Score: " + Mathf.FloorToInt(transform.position.z);
+        if (!isDead)
+        {
+            scoreText.text = "Score: " + Mathf.FloorToInt(transform.position.z);
+
+            if (Mathf.FloorToInt(transform.position.z) == 0)
+            {
+                speed = 3f;
+                scoreText.color = Color.green;
+            }
+            else if (Mathf.FloorToInt(transform.position.z) == 20)
+            {
+                speed = 4f;
+                scoreText.color = new Color((float)(0.333), 1, 0, 1);
+            }
+            else if (Mathf.FloorToInt(transform.position.z) == 40)
+            {
+                speed = 5f;
+                scoreText.color = new Color((float)(0.6156), 1, 0, 1);
+            }
+            else if (Mathf.FloorToInt(transform.position.z) == 60)
+            {
+                speed = 6f;
+                scoreText.color = new Color(1, 1, 0, 1);
+            }
+            else if (Mathf.FloorToInt(transform.position.z) == 80)
+            {
+                speed = 7f;
+                scoreText.color = new Color(1, (float)(0.6156), 0, 1);
+            }
+            else if (Mathf.FloorToInt(transform.position.z) == 100)
+            {
+                speed = 10f;
+                scoreText.color = Color.red;
+            }
+        }
 	}
 
 	void OnTriggerEnter (Collider otherCollider) {
 		if (otherCollider.tag == "Obstacle") {
             isDead = true;
             speed = 0f;
-			scoreText.text = "Dead!Score: " + Mathf.FloorToInt(transform.position.z);
+			scoreText.text = "Dead! Score: " + Mathf.FloorToInt(transform.position.z);
 			Invoke("reload",5);
 		}
 	}
